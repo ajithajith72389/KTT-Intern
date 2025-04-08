1a972a10f87782b0bddbc8590a27bd2dd12a6152
ajithajith72389 <ajithajith72389@gmail.com>
2025-04-08 11:27:14 +0530

    Variable scope, closure

--- Learning/JavaScript/advancedFunctions.js
+++ Learning/JavaScript/advancedFunctions.js
@@ -17,3 +17,46 @@ function fact(n){
     return n * fact(n-1);
 }
 console.log(fact(5));
+
+
+// scope
+
+{
+    let a = 1;
+    const b = 2;
+    var c = 3;
+}
+
+console.log("Block scope variables:");
+// console.log(a); // ReferenceError: a is not defined
+// console.log(b);
+console.log(c);
+
+
+console.log("Function");
+
+function simple(){
+    var simple_a= 1;
+    let simple_b = 2;
+    const simple_c = 3;
+    console.log(simple_a);
+    console.log(simple_b);
+    console.log(simple_c);
+}
+
+simple();
+
+//closure
+console.log("***Closure***");
+
+function outer(){
+    let count = 0;
+    return function inner(){
+        count++;
+        console.log(count);
+    }
+}
+const counter = outer()
+counter();
+counter(); 
+counter();
